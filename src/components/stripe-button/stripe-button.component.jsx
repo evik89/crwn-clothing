5d0091842630abaa9e85c4402c72ea4e630d6bfc
Evik Ghazarian <avikghazarian@Eviks-MBP.attlocal.net>
2022-01-05 11:05:29 -0800

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ( { price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51KEdA7Bn1FqGRlg25Uwl2c7B54KYfZQW2xCMwdQdXgyMdPrfGqh6zRlweem85oiKS7fJj8QGZNHWfbdsHkHuevjC00RMaqPzXw';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
